e15322bb4616b33f8909ec3769da64763d17d8ed
RenHao <765995472@qq.com>
2022-04-20 22:07:27 +0800

    commit by idea git1

---
+++ src/main/java/com/baoweidaima/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.baoweidaima.git;
+
+/**
+ * @ClassName GitTest
+ * @Deseription
+ * @Author RenHao
+ * @Date 2022/4/20 21:58
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git.");
+    }
+}
