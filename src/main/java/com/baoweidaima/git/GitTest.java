b7ad749ef87c49dc098435a7f1df5ae2a87fb2be
RenHao <765995472@qq.com>
2022-04-20 22:08:56 +0800

    commit by idea git2

--- src/main/java/com/baoweidaima/git/GitTest.java
+++ src/main/java/com/baoweidaima/git/GitTest.java
@@ -10,5 +10,6 @@ package com.baoweidaima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git.");
+        System.out.println("hello git 2");
     }
 }
